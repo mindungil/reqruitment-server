790781f6ba819ae7087cafc44befd56bb349a580
soet02 <alswnsrlf12@naver.com>
2024-12-02 16:14:31 +0900

    Feat: 기업정보 db 연동

--- src/dataBases/company.js
+++ src/dataBases/company.js
@@ -17,46 +17,40 @@ const start = async ()=> {
 
 start();
 
-const jobDataSchema = new mongoose.Schema({
+const companySchema = new mongoose.Schema( {
     회사명: String,
-    제목: String, // 공고에서 제목이 유일하므로, 공고 중복 방지
-    링크: String,
-    지역: String,
-    경력: String,
-    학력: String,
-    고용형태: String,
-    마감일: String,
-    직무분야: String,
-    기타정보: String,
-    갱신날짜: String,
-  });
+    채용중: Number,
+    기업규모: String,
+    사업분야: String,
+    존속기간: String,
+    CEO: String,
+    세부정보: String
+});
 
-jobDataSchema.index({제목: 1}, {unique: true});
+companySchema.index({회사명: 1}, {unique: true});
 
-const jobData = mongoose.model('job_data', jobDataSchema);
-  
-  // 파일 경로 설정
+const companyData = mongoose.model('company_data', companySchema);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = path.resolve(__dirname, '../../crawler/crawlingData/jobData.csv');
- 
-// 파일 존재 확인
+const filePath = path.resolve(__dirname, '../../crawler/crawlingData/saraminCrawlingCompany.csv');
+
 if (!fs.existsSync(filePath)) {
-  console.error('CSV 파일이 존재하지 않습니다:', filePath);
-  process.exit(1);
-}
-  
-const jobs = [];  // 데이터를 일괄 저장할 배열
+    console.error('CSV 파일이 존재하지 않습니다:', filePath);
+    process.exit(1);
+  }
+
+const companys = [];  // 데이터를 일괄 저장할 배열
   
   // CSV 파일 읽고 데이터 저장
 fs.createReadStream(filePath)
   .pipe(csv())
   .on('data', (row) => {
-    jobs.push(row);  // 일괄 저장을 위해 배열에 추가
+    companys.push(row);  // 일괄 저장을 위해 배열에 추가
   })
   .on('end', async () => {
     try {
-      await jobData.insertMany(jobs, {ordered: false});
+      await companyData.insertMany(companys, {ordered: false});
       console.log('모든 데이터가 저장되었습니다.');
     } catch (error) {
       if (error.code === 11000) {
