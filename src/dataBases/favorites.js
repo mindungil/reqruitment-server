dfa09b9080502a1d3855d160262d52f7c2a16b8a
soet02 <alswnsrlf12@naver.com>
2024-12-02 17:01:26 +0900

    Feat: 북마크 모델 스키마 구성

--- src/dataBases/favorites.js
+++ src/dataBases/favorites.js
@@ -5,16 +5,16 @@ const start = async () => {
     try {
         await mongodb();
         console.log("연결 성공");
-    } catch(err) {
+    } catch(err){
         console.error("연결 오류 : ", err);
         throw err;
     }
 };
 
-const reviewSchema = new mongoose.Schema({
-    회사: String,
-    작성자: String,
-    평점: Number,
-    평가: String,
+start();
+
+const favoriteSchema = new mongoose.Schema({
+    공고명: String,
+    등록자: String,
     등록날짜: String,
 });
