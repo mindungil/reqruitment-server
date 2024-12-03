78f316fe2a6daec435c4ff5dce99f53ba01120da
soet02 <alswnsrlf12@naver.com>
2024-12-03 09:59:06 +0900

    Chore: 경로 변경

--- src/dataBases/moveCsv/jobOpenings.js
+++ src/dataBases/moveCsv/jobOpenings.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import csv from 'csv-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { mongodb } from '../config/mongodb.js';
+import { mongodb } from '../../config/mongodb.js';
 
 const start = async ()=> {
   try{
@@ -17,40 +17,46 @@ const start = async ()=> {
 
 start();
 
-const companySchema = new mongoose.Schema( {
+const jobDataSchema = new mongoose.Schema({
     회사명: String,
-    채용중: Number,
-    기업규모: String,
-    사업분야: String,
-    존속기간: String,
-    CEO: String,
-    세부정보: String
-});
+    제목: String, // 공고에서 제목이 유일하므로, 공고 중복 방지
+    링크: String,
+    지역: String,
+    경력: String,
+    학력: String,
+    고용형태: String,
+    마감일: String,
+    직무분야: String,
+    기타정보: String,
+    갱신날짜: String,
+  });
 
-companySchema.index({회사명: 1}, {unique: true});
+jobDataSchema.index({제목: 1}, {unique: true});
 
-const companyData = mongoose.model('company_data', companySchema);
-
+const jobData = mongoose.model('job_data', jobDataSchema);
+  
+  // 파일 경로 설정
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = path.resolve(__dirname, '../../crawler/crawlingData/saraminCrawlingCompany.csv');
-
+const filePath = path.resolve(__dirname, '../../../crawler/crawlingData/jobData.csv');
+ 
+// 파일 존재 확인
 if (!fs.existsSync(filePath)) {
-    console.error('CSV 파일이 존재하지 않습니다:', filePath);
-    process.exit(1);
-  }
-
-const companys = [];  // 데이터를 일괄 저장할 배열
+  console.error('CSV 파일이 존재하지 않습니다:', filePath);
+  process.exit(1);
+}
+  
+const jobs = [];  // 데이터를 일괄 저장할 배열
   
   // CSV 파일 읽고 데이터 저장
 fs.createReadStream(filePath)
   .pipe(csv())
   .on('data', (row) => {
-    companys.push(row);  // 일괄 저장을 위해 배열에 추가
+    jobs.push(row);  // 일괄 저장을 위해 배열에 추가
   })
   .on('end', async () => {
     try {
-      await companyData.insertMany(companys, {ordered: false});
+      await jobData.insertMany(jobs, {ordered: false});
       console.log('모든 데이터가 저장되었습니다.');
     } catch (error) {
       if (error.code === 11000) {
